5789e2e62aa325241c1473663c452263987bda3f
SummitKwan <SummitKwan@hotmail.com>
2016-04-26 15:19:57 -0400

    Debug lowD embedding: for slicing sparse matrix, use np array, and avoid using pandas series, which is extremely slow

--- unsupervised/lowD_embedding.py
+++ unsupervised/lowD_embedding.py
@@ -10,8 +10,14 @@ import os
 import pandas as pd
 import json
 import numpy as np
+from scipy.sparse import csr_matrix, csc_matrix, lil_matrix
 import copy
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA, IncrementalPCA
+import sklearn.preprocessing
+import matplotlib
+import matplotlib.pyplot as plt
 
 # ========== get current file path and path to datafile ==========
 current_filepath = os.path.dirname(__file__)
@@ -35,12 +41,16 @@ def main():
     data = data[data['1'].apply(lambda x: x is not np.nan)]
 
     # (data_fullname, topic) = replace_topic_fullname(data)
-    [text_tf, vectorizer] = get_text_feature(data['abstract'][0:100])
+    [text_tf, vectorizer] = get_text_feature(data['abstract'])
+
+
+    embed_hierarchy(text_tf, data[['1','2','3']])
+
 
     return 'finish'
 
 
-# ========== read topic info, and add full name to data ==========
+# ---------- read topic info, and add full name to data ----------
 def replace_topic_fullname(data):
     with open(topic_filepath) as topicfile:
         topic = pd.read_csv(topicfile)
@@ -57,13 +67,93 @@ def replace_topic_fullname(data):
 
     return (data_fullname, topic)
 
-# ========== get text feature ==========
+
+# ---------- get text feature ----------
 def get_text_feature(corpus):
     # input is a list of strings
-    vectorizer = CountVectorizer(strip_accents='unicode',stop_words='english',min_df=1)
+
+    vectorizer = CountVectorizer(strip_accents='unicode',stop_words='english',min_df=5)
     text_tf = vectorizer.fit_transform(corpus)
+    text_tf = sklearn.preprocessing.normalize(text_tf.astype('float'),norm='l1', axis=1)
     return [text_tf, vectorizer]
 
+# ---------- group x according to y ----------
+def group_mean_x(x, y):
+    y = np.array(y)
+    yc = np.sort(np.unique(y)) # unique categories
+    C = len(yc)    # number of categories
+    [N,M] = x.shape
+    # xc = csc_matrix( np.zeros([C,M]) )
+    xc = np.zeros((C,M))
+
+    for i in range(C):
+        yi = yc[i]
+
+        # xc[i,:] = sparse_matrix_mean( x[y==yi, :] )
+        xc[i,:] = x[y==yi, :].mean(axis=0)  # explode memory because it converts sparse matrix to dense to do mean()
+    return [xc, yc]
+
+# ---------- tSNE embedding ----------
+def embed(x):
+    model = TSNE(n_components=2, random_state=0, perplexity=30.0, metric='correlation')
+    # model = IncrementalPCA(n_components=2)
+    return model.fit_transform(x)
+
+# ---------- low-D embed for the hierarchical data ----------
+def embed_hierarchy(x, yy):
+    [x1,y1] = group_mean_x(x, yy['1'])
+    [x2,y2] = group_mean_x(x, yy['2'])
+    [x3,y3] = group_mean_x(x, yy['3'])
+
+    # xh = x1
+    # yh = y1
+    # h  = np.ones(len(y1))
+
+    # xh = x3
+    # yh = y3
+    # h  = np.ones(len(y3))*3
+
+    # xh = np.concatenate((x1,x2))
+    # yh = np.concatenate((y1,y2))
+    # h  = np.concatenate((np.ones(len(y1))*1, np.ones(len(y2))*2))
+
+    xh = np.concatenate((x1,x2,x3))
+    yh = np.concatenate((y1,y2,y3))
+    h  = np.concatenate((np.ones(len(y1))*1, np.ones(len(y2))*2, np.ones(len(y3))*3))
+
+    # low-D embedding
+    print('start embedding')
+    xh_ld = embed(xh)
+
+    # color
+    mycolor = gen_distinct_color(len(y1))
+
+    y_to_c = dict(zip(y1, mycolor))
+    ch = [y_to_c[i] for i in [y[0] for y in yh]]
+
+
+    for i in range(len(y1)):
+        plt.scatter(xh_ld[i,0],xh_ld[i,1],s=300/h**3, c=ch[i], edgecolors='none' )
+    plt.legend( list(y1), loc='upper left', bbox_to_anchor=(1, 1) )
+    plt.scatter(xh_ld[:,0],xh_ld[:,1],s=300/h**3, c=ch, edgecolors='none' )
+
+
+    # return [xh, yh, h]
+
+# ---------- color for pretty plot ----------
+def gen_distinct_color(n):
+    # use golden ratio
+    golden_ratio_conjugate = 0.618033988749895
+
+    # use golden ratio
+    # h = np.arange(n, dtype='float') *golden_ratio_conjugate %1
+
+    # use spectrum
+    h = np.arange(n, dtype='float') / n + 0.5/n
+    distinct_color = []
+    for hi in h:
+        distinct_color.append( matplotlib.colors.hsv_to_rgb([hi,0.5,0.95]) )
+    return distinct_color
 
 if __name__ == '__main__':
   	main()
